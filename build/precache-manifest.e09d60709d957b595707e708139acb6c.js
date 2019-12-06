0c308a71c420fdb5cbb5f374278b6e62a348be54
igaryok <igor.sobko@gmail.com>
2019-12-06 18:18:07 +0200

    added buid

---
+++ build/precache-manifest.e09d60709d957b595707e708139acb6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b352a3cdec846a464c76b2d324cb5e9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "516c048778a1575281f3",
+    "url": "/static/css/main.188641d2.chunk.css"
+  },
+  {
+    "revision": "a5fef336e833ca5ea82d",
+    "url": "/static/js/2.cd51aa5a.chunk.js"
+  },
+  {
+    "revision": "516c048778a1575281f3",
+    "url": "/static/js/main.01e1f20f.chunk.js"
+  },
+  {
+    "revision": "0f894792c412f7e18c31",
+    "url": "/static/js/runtime-main.4be573e8.js"
+  }
+]);
